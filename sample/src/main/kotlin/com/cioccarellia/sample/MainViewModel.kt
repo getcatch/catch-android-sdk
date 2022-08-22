1fb6614f171c0eefc41b925150fb55c623802bba
Clark Sandholtz <clarksandholtz@gmail.com>
2022-08-22 10:31:03 -0700

    Creating initial project structure for Androd SDK (#1)

---
+++ sample/src/main/kotlin/com/cioccarellia/sample/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.cioccarellia.sample
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cioccarellia.lib.ComplexNumber
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+import kotlin.random.Random
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    private val _complexFlow = flow {
+        while (true) {
+            delay(150)
+            emit(
+                ComplexNumber(Random.nextInt(-100, 100), Random.nextInt(-100, 100))
+            )
+        }
+    }
+
+    val complexFLow = _complexFlow.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        ComplexNumber.origin()
+    )
+}
