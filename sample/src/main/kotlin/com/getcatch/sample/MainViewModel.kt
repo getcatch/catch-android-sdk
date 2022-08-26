a6c0436db2fadf015d15de265105febf57d947e5
Clark Sandholtz <clarksandholtz@gmail.com>
2022-08-26 15:21:25 -0700

    Adding detekt for static code analysis (#4)
    * Adding detekt for static code analysis
    
    * Testing github actions without build step

--- sample/src/main/kotlin/com/getcatch/sample/MainViewModel.kt
+++ sample/src/main/kotlin/com/getcatch/sample/MainViewModel.kt
@@ -9,12 +9,15 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
+private const val ONE_SECOND_IN_MILLIS = 1000L
+private const val FIVE_SECONDS_IN_MILLIS = 5 * ONE_SECOND_IN_MILLIS
+
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
     private val _complexFlow = flow {
         var counter = 0
         while (true) {
-            delay(1000)
+            delay(ONE_SECOND_IN_MILLIS)
             emit(
                 counter++
             )
@@ -23,7 +26,7 @@ class MainViewModel @Inject constructor() : ViewModel() {
 
     val complexFLow = _complexFlow.stateIn(
         viewModelScope,
-        SharingStarted.WhileSubscribed(5000),
+        SharingStarted.WhileSubscribed(FIVE_SECONDS_IN_MILLIS),
         0
     )
 }
