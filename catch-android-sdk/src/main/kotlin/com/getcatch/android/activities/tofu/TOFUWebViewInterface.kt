af2fcae06ec550e4edbb6ee816fdf405b82d9c70
Clark Sandholtz <clarksandholtz@gmail.com>
2022-10-14 12:16:18 -0700

    Refactor TOFUActivity to extend WebViewActivity

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/activities/tofu/TOFUWebViewInterface.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/activities/tofu/TOFUWebViewInterface.kt
@@ -1,40 +1,19 @@
 package com.getcatch.android.activities.tofu
 
 import android.util.Log
-import android.webkit.JavascriptInterface
-import android.widget.Toast
+import com.getcatch.android.activities.WebViewActivity
+import com.getcatch.android.web.CatchWebViewInterface
 import com.getcatch.android.web.PostMessageBody
-import kotlinx.serialization.SerializationException
 
-internal class TOFUWebViewInterface(private val activity: TOFUActivity) {
-
-    /** Show a toast from the web page  */
-    @JavascriptInterface
-    fun showToast(toast: String) {
-        Toast.makeText(activity, toast, Toast.LENGTH_SHORT).show()
-    }
-
-    @JavascriptInterface
-    fun handlePostMessage(message: String) {
-        if (message.isBlank() || message == EMPTY_JSON_STRING) {
-            return
-        }
-        try {
-            val body = PostMessageBody.fromJsonString(message)
-            if (body.action == "CATCH_TOFU_BACK") {
-                activity.finish()
-            }
-        } catch (ex: SerializationException) {
-            Log.e(
-                TAG,
-                "Error deserializing the following into a PostMessageBody: $message",
-                ex
-            )
+internal class TOFUWebViewInterface(activity: WebViewActivity) : CatchWebViewInterface(activity) {
+    override fun handlePostMessage(message: PostMessageBody) {
+        when (message.action) {
+            ACTION_BACK -> activity.finish()
+            else -> Log.d(this::class.simpleName, "Unhandled post message: $message")
         }
     }
 
     companion object {
-        private const val TAG = "TOFUWebViewInterface"
-        private const val EMPTY_JSON_STRING = "\"\""
+        private const val ACTION_BACK = "CATCH_TOFU_BACK"
     }
 }
