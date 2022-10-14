3b52183d7a33212a88758d79730e7a31e2019950
Clark Sandholtz <clarksandholtz@gmail.com>
2022-10-14 14:09:04 -0700

    Creating base classes for web view activities (#11)
    **Description**
    
    This PR introduces a new `WebViewActivity` that can be used as a base for other webview activities. It manages things like registering a listener for post messages and providing an API for sending post messages.
    
    **Open Questions**
    
    - It might end up being better for the url to not be generated within the activity, but before the activity is launched. I will make a call on that once I start using these a bit more in context.
    
    **Testing**
    
    **Relevant Links (e.g. Ticket)**
    
    **TODOs**

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/utils/UrlUtils.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/utils/UrlUtils.kt
@@ -1,9 +1,10 @@
 package com.getcatch.android.utils
 
+import android.net.Uri
 import io.ktor.http.URLBuilder
 
 /**
- * A utility method for building urls
+ * A utility method for building urls.
  *
  * @param path The url path (can be appended to in the block)
  * @param block A function block that allows you to call methods on the internal [URLBuilder]
@@ -14,3 +15,14 @@ internal fun buildUrl(path: String, block: URLBuilder.(URLBuilder) -> Unit): Str
         it.block(it)
         it.buildString()
     }
+
+/**
+ * A utility for extracting the base url from a [Uri] by stripping any params and/or fragments.
+ */
+internal val Uri.baseUrl: String
+    get() = Uri.Builder()
+        .scheme(this.scheme)
+        .authority(this.authority)
+        .path(this.path)
+        .build()
+        .toString()
