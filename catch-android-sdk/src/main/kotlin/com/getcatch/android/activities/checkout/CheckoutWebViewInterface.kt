2a855c70baa5556833334262ae00cd6d0d560dbb
Clark Sandholtz <clarksandholtz@gmail.com>
2022-10-14 14:28:21 -0700

    Creating Checkout webview activity (#13)
    **Description**
    
    The `CatchCheckoutActivity` currently doesn't do much at all, but should be able to load a valid checkout.
    
    **Open Questions**
    
    **Testing**
    
    **Relevant Links (e.g. Ticket)**
    
    **TODOs**

---
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/activities/checkout/CheckoutWebViewInterface.kt
@@ -0,0 +1,14 @@
+package com.getcatch.android.activities.checkout
+
+import android.util.Log
+import com.getcatch.android.activities.WebViewActivity
+import com.getcatch.android.web.CatchWebViewInterface
+import com.getcatch.android.web.PostMessageBody
+
+internal class CheckoutWebViewInterface(activity: WebViewActivity) : CatchWebViewInterface(activity) {
+    override fun handlePostMessage(message: PostMessageBody) {
+        when (message.action) {
+            else -> Log.d(this::class.simpleName, "Unhandled post message: $message")
+        }
+    }
+}
