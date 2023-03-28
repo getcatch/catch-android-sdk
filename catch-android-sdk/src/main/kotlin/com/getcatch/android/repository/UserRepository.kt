dd45220523f416e38344fd012d142da654b4c91b
Clark Sandholtz <clarksandholtz@gmail.com>
2023-03-28 11:11:39 -0700

    Create new UserRepository

---
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.getcatch.android.repository
+
+import com.getcatch.android.models.PublicUserData
+import kotlinx.coroutines.flow.StateFlow
+
+internal interface UserRepository {
+    val activeUser: StateFlow<PublicUserData?>
+    val didFetchUserData: StateFlow<Boolean>
+    var deviceToken: String?
+    suspend fun loadUserData(merchantId: String)
+}
---
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/repository/UserRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.getcatch.android.repository
+
+import com.getcatch.android.cache.CacheManager
+import com.getcatch.android.models.PublicUserData
+import com.getcatch.android.network.NetworkResponse
+import com.getcatch.android.network.clients.transactions.TransactionsSvcClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+internal class UserRepositoryImpl(
+    private val transactionsSvcClient: TransactionsSvcClient,
+    private val cache: CacheManager,
+) : UserRepository {
+    private val _activeUser = MutableStateFlow<PublicUserData?>(null)
+    override val activeUser: StateFlow<PublicUserData?> = _activeUser.asStateFlow()
+
+    private val _didFetchUserData = MutableStateFlow(false)
+    override val didFetchUserData: StateFlow<Boolean> = _didFetchUserData.asStateFlow()
+
+    override var deviceToken: String?
+        get() = cache.deviceToken
+        set(value) { cache.deviceToken = value }
+
+    override suspend fun loadUserData(merchantId: String) {
+        val token = deviceToken
+        if (token != null) {
+            val response =
+                transactionsSvcClient.fetchUserData(deviceToken = token, merchantId = merchantId)
+            when (response) {
+                is NetworkResponse.Success -> _activeUser.value = response.body
+                is NetworkResponse.Failure -> _activeUser.value = PublicUserData.noData
+            }
+        } else {
+            _activeUser.value = PublicUserData.noData
+        }
+
+        _didFetchUserData.value = true
+    }
+}
