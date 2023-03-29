979f80359bf041b2f94a2f92e2ab067d0ec7f576
Clark Sandholtz <clarksandholtz@gmail.com>
2023-03-29 10:40:16 -0700

    Adds a `CatchOptions` class and a tool for docs generation (#36)

---
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/CatchOptions.kt
@@ -0,0 +1,26 @@
+package com.getcatch.android
+
+import androidx.compose.ui.text.font.FontFamily
+import com.getcatch.android.network.Environment
+import com.getcatch.android.theming.ThemeVariant
+import com.getcatch.android.theming.ThemeVariantOption
+
+/**
+ * An object which specifies optional configuration settings to
+ * control the global behavior of the Catch SDK.
+ *
+ * @property colorTheme The default theme to be applied to all
+ * Catch widgets. Defaults to `ThemeVariant.Light`.
+ *
+ * @property environment The production environment should be used
+ * in live applications while the sandbox environment should be
+ * used for development and testing. Defaults to sandbox.
+ *
+ * @property customFontFamily An optional font family that will be
+ * applied to all text in widgets if set. Defaults to null.
+ */
+public data class CatchOptions(
+    val colorTheme: ThemeVariantOption = ThemeVariant.Light,
+    val environment: Environment = Environment.SANDBOX,
+    val customFontFamily: FontFamily? = null,
+)
