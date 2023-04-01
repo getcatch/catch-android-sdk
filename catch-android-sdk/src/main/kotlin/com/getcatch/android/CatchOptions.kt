89d430a0a9c33bfa6498f576cd18f172e5e80b11
Clark Sandholtz <clarksandholtz@gmail.com>
2023-04-01 16:10:55 -0700

    Adding CatchStyleConfig to the CatchOptions (#39)

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/CatchOptions.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/CatchOptions.kt
@@ -2,6 +2,7 @@ package com.getcatch.android
 
 import androidx.compose.ui.text.font.FontFamily
 import com.getcatch.android.network.Environment
+import com.getcatch.android.styling.CatchStyleConfig
 import com.getcatch.android.theming.ThemeVariant
 import com.getcatch.android.theming.ThemeVariantOption
 
@@ -18,9 +19,13 @@ import com.getcatch.android.theming.ThemeVariantOption
  *
  * @property customFontFamily An optional font family that will be
  * applied to all text in widgets if set. Defaults to null.
+ *
+ * @property styleConfig An optional style config that will be
+ * applied to all text in widgets if set. Defaults to null.
  */
 public data class CatchOptions(
     val colorTheme: ThemeVariantOption = ThemeVariant.Light,
     val environment: Environment = Environment.SANDBOX,
     val customFontFamily: FontFamily? = null,
+    val styleConfig: CatchStyleConfig? = null,
 )
