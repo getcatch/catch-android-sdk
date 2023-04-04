dece87a47b5b7250eb7c7005cdce87d2565bbc13
Clark Sandholtz <clarksandholtz@gmail.com>
2023-04-04 10:15:21 -0700

    Adding Saved to support sign up discount experiment (#49)

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/models/CalculatedReward.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/models/CalculatedReward.kt
@@ -1,25 +1,47 @@
 package com.getcatch.android.models
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.getcatch.android.R
 import com.getcatch.android.utils.centsToDollarsString
 import com.getcatch.android.utils.toPercentString
 
-internal sealed interface CalculatedReward {
-    val redeemable: Boolean
-    val formattedStringValue: String
+internal sealed class CalculatedReward {
+    abstract val redeemable: Boolean
+    abstract val formattedStringValue: String
+    abstract val stringResId: Int
 
-    class EarnedCredits(amount: Int): CalculatedReward {
+    class EarnedCredits(val amount: Int) : CalculatedReward() {
         override val redeemable: Boolean = false
         override val formattedStringValue: String = amount.centsToDollarsString()
+        override val stringResId: Int = R.string.benefit_earn_message
     }
 
-    class RedeemableCredits(amount: Int): CalculatedReward {
-        override val redeemable: Boolean = true
-        override val formattedStringValue: String = amount.centsToDollarsString()
-    }
-
-    class PercentRate(percent: Double): CalculatedReward {
+    class PercentRate(val percent: Double) : CalculatedReward() {
         override val redeemable: Boolean = false
         override val formattedStringValue: String = "${percent.toPercentString()}%"
+        override val stringResId: Int = R.string.benefit_earn_message
+    }
+
+    class RedeemableCredits(val amount: Int) : CalculatedReward() {
+        override val redeemable: Boolean = true
+        override val formattedStringValue: String = amount.centsToDollarsString()
+        override val stringResId: Int = R.string.benefit_redeem_message
+    }
+
+    class Saved(val amount: Int) : CalculatedReward() {
+        override val redeemable: Boolean = true
+        override val formattedStringValue: String = amount.centsToDollarsString()
+        override val stringResId: Int = R.string.benefit_saved_message
+    }
+
+    @Composable
+    fun getMessage(capitalize: Boolean = false): String {
+        val message = stringResource(stringResId, formattedStringValue)
+        if (capitalize) {
+            return message.replaceFirstChar { it.uppercase() }
+        }
+        return message
     }
 
     companion object {
