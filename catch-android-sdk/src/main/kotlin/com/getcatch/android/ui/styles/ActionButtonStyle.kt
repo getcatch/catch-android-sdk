e171f9af025eb21e8ff389e8af20481ab742169a
Clark Sandholtz <clarksandholtz@gmail.com>
2023-04-18 10:36:50 -0700

    Adding style resolution for action widgets (#69)

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/styles/ActionButtonStyle.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/styles/ActionButtonStyle.kt
@@ -1,8 +1,15 @@
 package com.getcatch.android.ui.styles
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.getcatch.android.ui.styles.values.ColorValue
 import com.getcatch.android.ui.styles.values.FontWeight
 import com.getcatch.android.ui.styles.values.TextTransform
+import androidx.compose.ui.text.font.FontWeight as ComposeFontWeight
 
 public data class ActionButtonStyle(
     val fontSize: Float? = null,
@@ -15,4 +22,60 @@ public data class ActionButtonStyle(
     val borderRadius: Float? = null,
     val backgroundColor: ColorValue? = null,
     val textTransform: TextTransform? = null,
-)
+) {
+    internal fun withOverrides(overrides: ActionButtonStyle?): ActionButtonStyle {
+        if (overrides == null) return this
+        return ActionButtonStyle(
+            fontSize = overrides.fontSize ?: fontSize,
+            fontColor = overrides.fontColor ?: fontColor,
+            height = overrides.height ?: height,
+            lineHeight = overrides.lineHeight ?: lineHeight,
+            fontWeight = overrides.fontWeight ?: fontWeight,
+            letterSpacing = overrides.letterSpacing ?: letterSpacing,
+            elevation = overrides.elevation ?: elevation,
+            borderRadius = overrides.borderRadius ?: borderRadius,
+            backgroundColor = overrides.backgroundColor ?: backgroundColor,
+            textTransform = overrides.textTransform ?: textTransform,
+        )
+    }
+
+    internal data class Resolved(
+        val fontSize: TextUnit,
+        val fontColor: Color,
+        val height: Dp,
+        val lineHeight: TextUnit,
+        val fontWeight: ComposeFontWeight,
+        val letterSpacing: TextUnit? = null,
+        val elevation: Dp,
+        val borderRadius: Dp,
+        val backgroundColor: Color,
+        val textTransform: TextTransform? = null,
+    ) {
+        fun withOverrides(overrides: ActionButtonStyle?): Resolved {
+            if (overrides == null) return this
+            return Resolved(
+                fontSize = overrides.fontSize?.sp ?: fontSize,
+                fontColor = overrides.fontColor?.value ?: fontColor,
+                height = overrides.height?.dp ?: height,
+                lineHeight = overrides.lineHeight?.sp ?: lineHeight,
+                fontWeight = overrides.fontWeight?.toComposeFontWeight() ?: fontWeight,
+                letterSpacing = overrides.letterSpacing?.sp ?: letterSpacing,
+                elevation = overrides.elevation?.dp ?: elevation,
+                borderRadius = overrides.borderRadius?.dp ?: borderRadius,
+                backgroundColor = overrides.backgroundColor?.value ?: backgroundColor,
+                textTransform = overrides.textTransform ?: textTransform,
+            )
+        }
+
+        val composeTextStyle = TextStyle(
+            color = fontColor,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            lineHeight = lineHeight,
+            letterSpacing = letterSpacing ?: TextUnit.Unspecified,
+        )
+
+        fun applyTextTransform(text: String) =
+            textTransform?.transform(text) ?: text
+    }
+}
--- catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/styles/ActionWidgetStyle.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/styles/ActionWidgetStyle.kt
@@ -1,7 +1,52 @@
 package com.getcatch.android.ui.styles
 
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+
 public data class ActionWidgetStyle(
     val widgetTextStyle: WidgetTextStyle? = null,
     val benefitTextStyle: BenefitTextStyle? = null,
     val actionButtonStyle: ActionButtonStyle? = null,
-)
+) {
+    internal fun withOverrides(overrides: ActionWidgetStyle?): ActionWidgetStyle {
+        if (overrides == null) return this
+        return ActionWidgetStyle(
+            widgetTextStyle = widgetTextStyle?.withOverrides(overrides.widgetTextStyle),
+            benefitTextStyle = benefitTextStyle?.withOverrides(overrides.benefitTextStyle),
+            actionButtonStyle = actionButtonStyle?.withOverrides(overrides.actionButtonStyle),
+        )
+    }
+
+    internal data class Resolved(
+        val widgetTextStyle: WidgetTextStyle.Resolved,
+        val benefitTextStyle: BenefitTextStyle.Resolved,
+        val actionButtonStyle: ActionButtonStyle.Resolved,
+    ) {
+        fun withOverrides(overrides: ActionWidgetStyle?): Resolved {
+            if (overrides == null) return this
+            return Resolved(
+                widgetTextStyle = widgetTextStyle.withOverrides(overrides.widgetTextStyle),
+                benefitTextStyle = benefitTextStyle.withOverrides(overrides.benefitTextStyle),
+                actionButtonStyle = actionButtonStyle.withOverrides(overrides.actionButtonStyle),
+            )
+        }
+
+        val composeTextStyle = TextStyle(
+            color = widgetTextStyle.fontColor,
+            fontSize = widgetTextStyle.fontSize,
+            fontWeight = widgetTextStyle.fontWeight,
+            lineHeight = widgetTextStyle.lineHeight,
+            letterSpacing = widgetTextStyle.letterSpacing ?: TextUnit.Unspecified,
+        )
+
+        val earnedComposeSpanStyle = SpanStyle(
+            color = benefitTextStyle.earnFontColor,
+            fontWeight = benefitTextStyle.fontWeight,
+        )
+
+        fun applyTextTransform(text: String) =
+            widgetTextStyle.textTransform?.transform(text) ?: text
+
+    }
+}
