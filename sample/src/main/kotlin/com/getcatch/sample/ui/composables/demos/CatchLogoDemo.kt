8abde6522d44f20f9ffb25afea224b4e1b638f64
Clark Sandholtz <clarksandholtz@gmail.com>
2023-05-10 10:58:48 -0600

    Creating demo composables for the logo widget and checkout (#115)
    * Creating SegmentedControl composable
    
    * Creating a scaffold component for the Demo app and global settings controls
    
    * Creating DemoSection composable
    
    * Creating demo composables for all InfoWidgets
    
    * Creating demo composables for all action widgets
    
    * Creating demo composables for the logo widget and checkout

---
+++ sample/src/main/kotlin/com/getcatch/sample/ui/composables/demos/CatchLogoDemo.kt
@@ -0,0 +1,32 @@
+package com.getcatch.sample.ui.composables.demos
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import com.getcatch.android.ui.composables.CatchLogo
+import com.getcatch.android.ui.composables.CatchLogoSize
+import com.getcatch.sample.ui.composables.DemoSection
+import com.getcatch.sample.ui.composables.SegmentedControl
+import com.getcatch.sample.utils.capitalize
+
+@Composable
+fun CatchLogoDemo() {
+    var logoSize: CatchLogoSize by remember { mutableStateOf(CatchLogoSize.SMALL) }
+    DemoSection(title = "Catch Logo", widgetContent = {
+        CatchLogo(size = logoSize)
+    }, settingsContent = {
+        Column(verticalArrangement = Arrangement.spacedBy(6.dp)) {
+            Text("Border Style")
+            SegmentedControl(
+                options = CatchLogoSize.values().toList().map { it.name.lowercase().capitalize() },
+                onOptionSelected = { logoSize = CatchLogoSize.values()[it] }
+            )
+        }
+    })
+}
