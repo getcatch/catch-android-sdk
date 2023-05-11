ef9a1c44981bab2b58baa11f9371894a82db64dc
Clark Sandholtz <clarksandholtz@gmail.com>
2023-05-11 11:42:58 -0600

    Docs for payment method widgets

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/views/PaymentMethodView.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/views/PaymentMethodView.kt
@@ -16,37 +16,75 @@ import com.getcatch.android.ui.theming.ThemeVariant
 import com.getcatch.android.utils.getPaymentMethodVariant
 import com.getcatch.android.utils.getThemeVariant
 
+/**
+ * The PaymentMethod widget displays similar messaging and informational content as the Callout,
+ * but is designed specifically to be displayed in checkout UI where a consumer may select Catch as
+ * their payment method.
+ *
+ * The widget may also be placed in a "disabled" state when the cart contains items that cannot be
+ * purchased with Catch (e.g. gift cards). The disable state grays out the contents of the widget
+ * and provides a small tooltip that explains why Catch cannot be selected.
+ *
+ * The PaymentMethod widget also makes use of the price, items, and userCohorts attributes to
+ * calculate rewards the user will earn on the current purchase.
+ */
 public class PaymentMethodView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : AbstractComposeView(context, attrs, defStyleAttr) {
     private val _price = mutableStateOf<Int?>(null)
+    private val _items = mutableStateOf<List<Item>?>(null)
+    private val _userCohorts = mutableStateOf<List<String>?>(null)
+    private val _variant = mutableStateOf<PaymentMethodVariant>(PaymentMethodVariant.Standard)
+    private val _disabled = mutableStateOf(false)
+    private val _theme = mutableStateOf<ThemeVariant?>(null)
+    private val _styleOverrides = mutableStateOf<InfoWidgetStyle?>(null)
+
+    /**
+     * The cost in cents that a consumer would pay for the item(s)
+     * without redeeming Catch credit.
+     */
     public var price: Int? by _price
 
-    private val _variant = mutableStateOf<PaymentMethodVariant>(PaymentMethodVariant.Standard)
-    public var variant: PaymentMethodVariant by _variant
-
-    private val _disabled = mutableStateOf(false)
-    public var disabled: Boolean by _disabled
-
-    private val _themeVariant = mutableStateOf<ThemeVariant?>(null)
-    public var themeVariant: ThemeVariant? by _themeVariant
-
-    private val _items = mutableStateOf<List<Item>?>(null)
+    /** A list of all items included in the order. Used to calculate item-based rewards. */
     public var items: List<Item>? by _items
 
-    private val _userCohorts = mutableStateOf<List<String>?>(null)
+    /**
+     * A list of user cohorts that the signed in user qualifies for.
+     * Used to calculate cohort-based rewards.
+     */
     public var userCohorts: List<String>? by _userCohorts
 
-    private val _styleOverrides = mutableStateOf<InfoWidgetStyle?>(null)
+    /** Whether or not the widget is in a disabled state. */
+    public var disabled: Boolean by _disabled
+
+    /**
+     * The PaymentMethod has several variants that allow you to customize what content
+     * is rendered in the widget. The [PaymentMethodVariant] options are:
+     *  - [PaymentMethodVariant.Standard] - displays the logo, filler text, and reward text
+     *  - [PaymentMethodVariant.Compact] - displays the filler text and reward text
+     *  - [PaymentMethodVariant.LogoCompact] - displays the logo and reward text
+     */
+    public var variant: PaymentMethodVariant by _variant
+
+    /**
+     * The Catch color [ThemeVariant]. If no theme is set, the theme set globally on the
+     * [Catch] object will be used, which defaults to [ThemeVariant.Light].
+     */
+    public var theme: ThemeVariant? by _theme
+
+    /**
+     * Style overrides which can be used to override the widget's default
+     * appearance (ex. font size, color, weight, etc.).
+     */
     public var styleOverrides: InfoWidgetStyle? by _styleOverrides
 
     init {
         context.theme.obtainStyledAttributes(attrs, R.styleable.PaymentMethodView, 0, 0).apply {
             try {
                 getPaymentMethodVariant()?.let { _variant.value = it }
-                _themeVariant.value = getThemeVariant(R.styleable.PaymentMethodView_themeVariant)
+                _theme.value = getThemeVariant(R.styleable.PaymentMethodView_themeVariant)
             } finally {
                 recycle()
             }
@@ -61,7 +99,7 @@ public class PaymentMethodView @JvmOverloads constructor(
             userCohorts = _userCohorts.value,
             disabled = _disabled.value,
             variant = _variant.value,
-            theme = _themeVariant.value,
+            theme = _theme.value,
             styleOverrides = _styleOverrides.value,
         )
     }
