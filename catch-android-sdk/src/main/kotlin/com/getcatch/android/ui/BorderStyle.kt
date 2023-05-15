1f738a3d52988e7700afd16c67ccf6565db906c2
Clark Sandholtz <clarksandholtz@gmail.com>
2023-05-15 13:32:21 -0700

    Adding documentation to border styles (#132)

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/BorderStyle.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/BorderStyle.kt
@@ -2,15 +2,20 @@ package com.getcatch.android.ui
 
 import com.getcatch.android.ui.styles.values.ColorValue
 
+/** The options for the style of border the widgets render.*/
 public sealed interface BorderStyle {
 
+    /** Renders a border with square corners. */
     public object Square : BorderStyle
 
+    /** Renders a border with slightly rounded corners. */
     public object SlightRound : BorderStyle
 
+    /** Renders a border in the specified color and with the specified radius. */
     public class Custom(
         internal val color: ColorValue, internal val radius: Float = 0f
     ) : BorderStyle
 
+    /** Renders widgets with no borders and no padding around the internal content. */
     public object None : BorderStyle
 }
--- catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/CalloutBorderStyle.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/ui/CalloutBorderStyle.kt
@@ -2,17 +2,28 @@ package com.getcatch.android.ui
 
 import com.getcatch.android.ui.styles.values.ColorValue
 
-
+/**
+ * The options for the style of border the [Callout] widget renders.
+ *
+ * The only difference between this and the [BorderStyle] is that the [Callout] widget
+ * also supports a pill shaped border.
+ */
 public sealed interface CalloutBorderStyle {
 
+    /** Renders a border with square corners. */
     public object Square : CalloutBorderStyle
+
+    /** Renders a border with slightly rounded corners. */
     public object SlightRound : CalloutBorderStyle
 
+    /** Renders a border with fully rounded corners. */
     public object Pill : CalloutBorderStyle
 
+    /** Renders a border in the specified color and with the specified radius. */
     public class Custom(
         internal val color: ColorValue, internal val radius: Float = 0f
     ) : CalloutBorderStyle
 
+    /** Renders widgets with no borders and no padding around the internal content. */
     public object None : CalloutBorderStyle
 }
