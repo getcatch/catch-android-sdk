f9398c77a1bd75fb21a24935b07de5c576438956
Clark Sandholtz <clarksandholtz@gmail.com>
2023-05-24 10:36:36 -0700

    Fixing default reward code

--- catch-android-sdk/src/main/kotlin/com/getcatch/android/models/CalculateRewardsResult.kt
+++ catch-android-sdk/src/main/kotlin/com/getcatch/android/models/CalculateRewardsResult.kt
@@ -5,8 +5,15 @@ internal data class CalculateRewardsResult(
     val rewardsSummary: EarnedRewardsSummary?,
 ) {
     companion object {
+        /** Fallback for when no merchant is loaded. */
         val NoRewardsSummary = CalculateRewardsResult(
             reward = CalculatedReward.Default,
+            rewardsSummary = null,
+        )
+
+        /** Fallback for when no rewards summary can be loaded/generated. */
+        fun merchantDefaultRewardSummary(merchant: Merchant) = CalculateRewardsResult(
+            reward = CalculatedReward.PercentRate(merchant.defaultEarnedRewardsRate),
             rewardsSummary = null,
         )
     }
